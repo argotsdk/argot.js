a9f990004026148c0f53e76765fba3363ac39a94
Dan Midwood <dan@danmidwood.com>
2014-02-15 18:43:48 -0500

    Add support for Argot Message
    The Argot Message format allows the definition for a type to be bundled
    alongside the data in a message payload with no prior definition loading
    required, this change adds support for reading the message.
    
    The process for reading an incoming payload is:
    * Check the magic number is correct (65)
    * Read the version data in the payload (for now only 1.3 is supported)
    * Use the version data to internally load dictionaries to create common
      and meta libraries
    * Copy a few important definitions from the common library to the meta
      library (and then discard the common library). The meta
      library (hereafter just known as library) is then used as a base for
      reading the payload
    * Read the definitions in the payload and add them to the library
    * Read the payload data and build the type according to the definitions
    
    See the blog post for full details on the format
    http://blog.argot-sdk.org/2014/02/argot-message-format-self-describing.html

---
+++ test/readMessage_test.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ These tests take the data written in the java test case and in the format as defined in
+ com.argot.data.MixedDataLoader, in which the names 'short' and 'byte' are used.
+ These words, being reserved in js, should not be used. Ideally, these words would not be
+ use in the data description, but with reality being far from ideal it would be prudent
+ to implement an additional step providing a definition-name to object-property-name
+ mapping.
+ Failing that, we could hold a list of reserved words and, when encountered apply a kludge,
+ possibly a underscore prefix or named as aType.
+*/
+
+var logger = require('winston');
+logger.cli();
+var streamifier = require('streamifier');
+
+var argot = require('../lib/argot.js');
+
+exports['read_test_message'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(3);
+    var input = [65, 19, 1, 50, 32, 0, 4, 100, 97, 116, 97, 1, 0, 27, 15, 3, 14, 5, 115, 104, 111, 114, 116, 13, 40, 14, 4, 98, 121, 116, 101, 13, 1, 14, 4, 116, 101, 120, 116, 13, 8, 50, 0, 10, 50, 5, 104, 101, 108, 108, 111];
+
+    var fileStream_input = streamifier.createReadStream (new Buffer(input));
+    setTimeout(argot.readMessage(fileStream_input,
+                                 function(read_data) {
+                                   // tests here
+                                   test.equals(read_data.short, 10);
+                                   test.equals(read_data.byte, 50);
+                                   test.equals(read_data.text, 'hello');
+                                   test.done();
+                                 },
+                                 function() {
+                                   // if we arrive here the test fails because the expected number of
+                                   // of assertions were not applied. TODO Find out how to fail with
+                                   // a useful error message instead
+                                   test.done();
+                                 }
+                                ),1000);
+  }
+};
+
+exports['read_test_message_invalid_magic_number'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(0);
+    var input = [65, 18];
+
+    var fileStream_input = streamifier.createReadStream (new Buffer(input));
+    setTimeout(argot.readMessage(fileStream_input,function() {
+      // tests here
+      test.fail();
+    },function() {
+      test.done();
+    }),1000);
+  }
+};
